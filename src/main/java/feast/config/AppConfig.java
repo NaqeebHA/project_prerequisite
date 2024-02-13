d00f1bd8a1ae48231e2a007f1a87377063554f9b
NaqeebHA <naqeebha23@gmail.com>
2024-02-13 11:36:41 +0800

    solution

--- src/main/java/feast/config/AppConfig.java
+++ src/main/java/feast/config/AppConfig.java
@@ -1,7 +1,6 @@
 package feast.config;
 
-import feast.models.Chicken3;
-import feast.models.Duck2;
+import feast.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +12,15 @@ public class AppConfig {
     @Bean
     public static Duck2 getDuck(Chicken3 chicken) {
         return new Duck2(chicken);
+    }
+
+    @Bean
+    public static Pig4 getPig(Lamb5 lamb) {
+        return new Pig4(lamb);
+    }
+
+    @Bean
+    public static Horse7 getHorse(Camel8 camel) {
+        return new Horse7(camel);
     }
 }
--- src/main/java/feast/models/Horse7.java
+++ src/main/java/feast/models/Horse7.java
@@ -1,8 +1,23 @@
 package feast.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Horse7 {
+    private Camel8 camel;
+
+    public Horse7(Camel8 camel) {
+        this.camel = camel;
+    }
+
     @Override
     public String toString() {
-        return "which is stuffed inside a camel, ";
+        return "which is stuffed inside a camel, " + camel.toString();
+    }
+
+    @Autowired
+    public void setCamel(Camel8 camel) {
+        this.camel = camel;
     }
 }
